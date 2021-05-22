42ba462c09dc2548d0a9fd6bb0b9bdc70501d1c2
Dmitry Krol <kroldm@gmail.com>
2021-05-22 20:21:02 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { HomeRoute, EilatRoute } from './routes';
 import TopBar from './TopBar';
 
 import WhatsAppIcon from '@material-ui/icons/WhatsApp';
+import MailIcon from '@material-ui/icons/Mail';
 
 const App = () => {
 
@@ -15,11 +16,17 @@ const App = () => {
   return (
     <BrowserRouter>
       <TopBar>
-        <a target="_blank" title="Contact Us On WhatsApp" href="https://api.whatsapp.com/send?phone=972526881816" style={{color: "white", textDecoration: "none"}} >
-        <div>
-          <WhatsAppIcon />
-          <span>+972-52-688-1816 (Екатерина)</span>
-        </div>
+        <a target="_blank" rel="noreferrer" title="Свяжитесь с нами по WhatsApp" href="https://api.whatsapp.com/send?phone=972526881816" style={{color: "white", textDecoration: "none"}} >
+          <div>
+            <WhatsAppIcon />
+            <span>+972-52-688-1816 (Екатерина)</span>
+          </div>
+        </a>
+        <a target="_blank" rel="noreferrer" title="Свяжитесь с нами по Email" href="mailto: ekaterina.krol@gmail.com" style={{color: "white", textDecoration: "none"}} >
+          <div style={{marginLeft: 10}}>
+            <MailIcon />
+            <span>ekaterina.krol@gmail.com</span>
+          </div>
         </a>
       </TopBar>
       {HomeRoute}
